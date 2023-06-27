ab603bc52c613ec4d6c853a1b0569655327cfe07
venkata shiva narayana modini <shivanarayana@users.noreply.github.com>
2023-06-27 18:09:28 +0530

    first commit

---
+++ src/main/java/com/example/AWSSDK/AwsSdkJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.AWSSDK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSdkJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSdkJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AWSSDK/AwsSdkJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AWSSDK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSdkJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
